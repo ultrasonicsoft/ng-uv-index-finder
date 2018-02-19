542a51de3199e920a4c0dcf5101d4689d76c7e3d
ultrasonicsoft <balramchavan@gmail.com>
2018-02-19 13:43:08 +0530

    source code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  uvIndex: number;
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit() {    
+  }
+
+  findMe() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.showUVIndex(position);
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+  showUVIndex(position) {
+
+    let url = `https://api.openweathermap.org/data/2.5/uvi?appid=715fb6755c8593cd50a03d7189d750dc&lat=${position.coords.latitude}&lon=${position.coords.longitude}`;
+    this.httpClient.get(url).subscribe(data => {
+      this.uvIndex = (<any>data).value;
+      console.log(data);
+    })    
+  }
 }
